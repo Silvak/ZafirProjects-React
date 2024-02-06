a7cf55f2820ebedc447e91beb4a7a48c649ece09
silvak <silvak.jeg@gmail.com>
2024-02-06 07:37:29 -0400

    init template

---
+++ src/vite-env.d.ts
@@ -0,0 +1,25 @@
+declare module "@/components/navBar/navItems";
+declare module "@/components/navBar/sidebar";
+declare module "@/components/navBar/sidebarUser";
+declare module "@/components/navBar/sidebar";
+declare module "@/components/navBar/navBarDrawer";
+declare module "@/components/buttons/buttonPrimary";
+declare module "@/components/inputs/inputSearchNavbar";
+declare module "@/components/alert/alert";
+declare module "@/components/modal/modal";
+
+declare module "@/context/AppContextProvider";
+declare module "@/stores/index";
+declare module "@/stores/container/container";
+declare module "@/stores/Actions/Theme/storeTheme";
+declare module "@/stores/useGetNativeBalance/useGetNativeBalance";
+declare module "@/stores/useGetWalletTokenBalances/useGetWalletTokenBalances";
+
+declare module "@/routes/routes";
+
+declare module "@/screens/home";
+declare module "@/screens/notFoundPage";
+
+declare module "@/theme/theme";
+
+/// <reference types="vite/client"/>
