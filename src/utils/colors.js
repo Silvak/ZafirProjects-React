27f0875e54cb5baaaefdf479017f36762a18a726
francorinque <francoagustinrinquecorreo@gmail.com>
2024-02-20 14:00:15 -0300

    fix(detailTask): fixed styles

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,28 +1,3 @@
-// export const getStatusColor = (status) => {
-//   switch (status) {
-//     case "Pending":
-//       return {
-//         bg: "#F6F7FA",
-//         color: "#6B6E75",
-//       };
-//     case "In progress":
-//       return {
-//         bg: "#f6b58a",
-//         color: "#D49265",
-//       };
-//     case "Completed":
-//       return {
-//         bg: "#E2F3F0",
-//         color: "#429482",
-//       };
-//     default:
-//       return {
-//         bg: "#FFECB3",
-//         color: "#8B4513",
-//       };
-//   }
-// };
-
 export const statusColors = {
   "In Progress": { backgroundColor: "#BED8F5", color: "#2676CF" },
   Issues: { backgroundColor: "#F8D1CB", color: "#D3544B" },
