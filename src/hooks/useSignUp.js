49fcf805d101752e21d5659ca9720e2f519ce6e5
Wilder Ap <wilderasprilla321@gmail.com>
2024-03-28 01:53:38 -0500

    Basic Register frontend to backend connected

--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -1,18 +1,42 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useBoundStore } from "@/stores/index";
+import axios from "axios";
 
 function useSignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setConfirmPassword] = useState(false);
 
-  const { setDataPerfilUser, setUser, setAuthenticated } = useBoundStore(
+  const { setDataPerfilUser, setUser, setAuthenticated, User } = useBoundStore(
     (state) => state
   );
 
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
+  const register = async (user) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/user/register",
+        user
+      );
+      console.log(user);
+      if (response.status === 201) {
+        console.log(response.data.message);
+        setAuthenticated(true);
+        navigate("/");
+      } else {
+        throw new Error("Error al registrar usuario");
+      }
+      if (response.status === 400) {
+        alert("El usuario ya existe");
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
@@ -34,8 +58,9 @@ function useSignUp() {
 
     setDataPerfilUser(user);
     setUser(user);
-    setAuthenticated(true);
-    navigate("/sign-in");
+    register(user);
+    // setAuthenticated(true);
+    // navigate("/sign-in");
   };
 
   const handleClickShowPassword = () => setShowPassword(!showPassword);
