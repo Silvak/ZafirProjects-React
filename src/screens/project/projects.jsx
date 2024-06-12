2d12d9c247b6fdc01e5436cc470690069833166c
Benjamín <benser22@hotmail.com>
2024-06-12 16:49:22 -0300

    Add(button): create project button added to All Projects screen

--- src/screens/project/projects.jsx
+++ src/screens/project/projects.jsx
@@ -1,12 +1,57 @@
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, Grid, Button } from '@mui/material';
 import ProjectsTable from '@/components/projectsTable/ProjectsTable';
+import { useBoundStore } from '../../stores';
+import AddIcon from '@mui/icons-material/Add';
+import { shallow } from 'zustand/shallow';
+import CreateProjectForm from '@/components/forms/CreateProjectForm';
 
 const projects = () => {
+  const { ChangeStateModal, ChangeContentModal, ChangeTitleModal } =
+    useBoundStore((state) => state, shallow);
+
+  const handleCreate = () => {
+    ChangeTitleModal('Add Project');
+    ChangeContentModal(<CreateProjectForm />);
+    ChangeStateModal(true);
+  };
+
   return (
-    <Box sx={{ padding: '50px 0', width: '100%' }}>
-      <Typography variant='h2' sx={{ mb: '70px' }}>
-        All Projects
-      </Typography>
+    <Box
+      sx={{
+        padding: '50px 0',
+      }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          mb: '70px',
+        }}
+      >
+        <Typography variant="h2">All Projects</Typography>
+        <Grid>
+          <Button
+            onClick={handleCreate}
+            sx={{
+              textTransform: 'none',
+              color: 'white',
+              backgroundColor: '#7662EA',
+              height: '40px',
+              minWidth: 'max-content',
+              borderRadius: '12px',
+              fontSize: '13px',
+              fontWeight: 500,
+              fontFamily: 'Poppins',
+              '&:hover': { backgroundColor: 'black' },
+            }}
+          >
+            <AddIcon sx={{ marginRight: '10px' }} />
+            Create new
+          </Button>
+        </Grid>
+      </Box>
       <ProjectsTable />
     </Box>
   );
